91ab1a1679d745dd9ceec62d25a886312b4bf9ad
David Freedus <dfreedus@penlink.com>
2021-05-31 22:07:15 -0500

    AlsoEnergyInc Coding Challenge

---
+++ Challenge/ChallengeTest/PermissionSet.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+
+namespace ChallengeTest
+{
+    [ProtoContract(SkipConstructor = true)]
+    public class PermissionSet
+    {
+        [ProtoMember(1)]
+        public bool[] UserPermissions = new bool[100];
+
+        //For testing serialization
+        public void SetPermissions()
+        {
+            for (int i = 0; i < 100; i++)
+                 UserPermissions[i] = i % 2 == 0;
+        }
+    }
+}
